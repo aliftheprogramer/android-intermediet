217b002c4298a64392432db6a6cf5aa0b1a19adc
Alif <alif.5220411242@student.uty.ac.id>
2024-11-13 01:31:45 +0700

    mantsppp

---
+++ TicketingApps/app/src/main/java/com/example/ticketingapps/data/Seat.kt
@@ -0,0 +1,11 @@
+package com.example.ticketingapps.data
+
+data class Seat(
+    val id : Int,
+    var x: Float? = 0F,
+    var y: Float? = 0F,
+    var name: String,
+    var isBooked: Boolean
+)
+
+
